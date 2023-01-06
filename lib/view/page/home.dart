6142ef0c49d3bb8ad12689838df6c3fc6f708b8c
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-01-06 16:32:29 +0600

    add side drawer

--- lib/view/page/home.dart
+++ lib/view/page/home.dart
@@ -9,9 +9,92 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: const Text("Home"),
       ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children:  [
+            SizedBox(
+              height: 230,
+              child: DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Column(
+                  children: [
+                    const Expanded(
+                      child: FittedBox(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("images/sukanto_profile_pic.jpg")
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: const Text(
+                        "Sukanto Saha",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            MenuItem(
+              name: "Home",
+              icon: Icons.home_outlined,
+              onTap: (){
+              }
+            ),
+            MenuItem(
+              name: "Profile",
+              icon: Icons.person_outline,
+              onTap: (){
+              }
+            ),
+            MenuItem(
+              name: "Logout",
+              icon: Icons.logout_outlined,
+              onTap: (){
+              }
+            ),
+            const Divider(color: Colors.black,),
+            MenuItem(
+              name: "Register",
+              icon: Icons.add_outlined,
+              onTap: (){
+              }
+            ),
+          ],
+        ),
+      ),
       body: const Center(
         child: Text("Welcome to home"),
       ),
     );
   }
+}
+
+class MenuItem extends StatelessWidget{
+  const MenuItem({required this.name, required this.icon, required this.onTap, super.key});
+  final String name;
+  final IconData icon;
+  final VoidCallback onTap; 
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        name,
+        style: const TextStyle(
+          fontSize: 17
+        ),
+      ),
+      leading: Icon(icon),
+      onTap: onTap,
+    );
+  }
+
 }
