d275039ab13aa1330fe490b8108117ff258c3071
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-01-08 21:29:00 +0600

    setup profile page

--- lib/view/page/profile.dart
+++ lib/view/page/profile.dart
@@ -8,9 +8,61 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profile"),
+        automaticallyImplyLeading: false,
       ),
-      body: const Center(
-        child: Text("Profile page"),
+      body:  Center(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              margin: const EdgeInsets.only(
+                top: 20,
+              ),
+              child: const FittedBox(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("images/sukanto_profile_pic.jpg")
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: const Text(
+                "Sukanto Saha",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: const Text(
+                "Hi, I'm Sukanto saha, I'm M.Sc. student of Rajshahi "
+                "University. I completed my graguation in pure "
+                "mathematics from Rajshahi University.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 18
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.arrow_back),
+                  Text("Go Back"),
+                ],
+              )
+            )
+          ],
+        ),
       ),
     );
   }
