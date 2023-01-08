c7466a0ec205281a95686a0703f893c3b93a573f
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-01-09 01:08:59 +0600

    design login page

--- lib/view/page/login.dart
+++ lib/view/page/login.dart
@@ -9,8 +9,94 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Login"),
       ),
-      body: const Center(
-        child: Text("Login page"),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Card(
+                margin: const EdgeInsets.fromLTRB(30, 30, 30, 80),
+                elevation: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 40),
+                        child: const Text(
+                          "LOGIN",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        decoration: const InputDecoration(
+                          hintText: 'Your Name',
+                          labelText: 'NAME *',
+                        ),
+                        validator: (String? value) {
+                          return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                        },
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 50),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Password',
+                            labelText: 'PASSWORD *',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 60,
+                left: 60,
+                right: 60,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(100, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)
+                    )
+                  ),
+                  child: const Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                )
+              ),
+
+              Positioned(
+                bottom: 0,
+                left: 60,
+                right: 60,
+                child: TextButton(
+                  onPressed: (){},
+                  child: const Text("Forgot password?"),
+                )
+              ),
+
+            ],
+            
+            // alignment: Alignment.center,
+          ),
+        ),
       ),
     );
   }
