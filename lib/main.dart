43d3d939d2ce318cd594f625a53f7300d5302bcf
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-01-12 01:31:50 +0600

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:profile_app/user_manager/user_manager.dart';
-import 'package:profile_app/view/page/login.dart';
-import 'view/page/home.dart';
+import 'package:profile_app/view/page/splash.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,17 +15,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FutureBuilder<User?>(
-        future: UserManager.getCurrentUser(),
-        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-          if(snapshot.hasData){
-            if(snapshot.data != null){
-              return const HomePage();
-            }
-          }
-          return const LoginPage();
-        },
-      )
+      home: const SplashPage(),
     );
   }
 }
---
+++ lib/view/page/splash.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:profile_app/user_manager/user_manager.dart';
+import 'package:profile_app/view/page/login.dart';
+
+import 'home.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero,() async {
+      User? user = await UserManager.getCurrentUser();
+      if(!mounted) return;
+
+      if(user != null){
+        Navigator.pop(context);
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context)=> const HomePage())
+        );
+      }else{
+        Navigator.pop(context);
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context)=> const LoginPage())
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(     
+      alignment: Alignment.center,                 
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.blue, 
+            Color.fromARGB(255, 115, 30, 227)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )
+      ),
+      child: const SizedBox(
+        height: 100,
+        child: FittedBox(
+          child: CircleAvatar(                            
+            backgroundColor: Colors.white,
+            child: Icon(Icons.home),
+          ),
+        ),
+      ),
+    );
+  }
+}
