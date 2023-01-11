e66cfe547b3ccf7634a17223554b8583914836ff
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-01-12 00:34:23 +0600

    show loginpage if no user logined

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:profile_app/user_manager/user_manager.dart';
+import 'package:profile_app/view/page/login.dart';
 import 'view/page/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +22,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: FutureBuilder<User?>(
+        future: UserManager.getCurrentUser(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if(snapshot.hasData){
+            if(snapshot.data != null){
+              return const HomePage();
+            }
+          }
+          return const LoginPage();
+        },
+      )
     );
   }
 }
