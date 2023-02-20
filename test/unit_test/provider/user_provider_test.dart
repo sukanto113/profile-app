f25e8ca91e5da1b94b98bd374f5b90973c624e72
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-02-20 12:28:52 +0600

    add more userProvider test

--- test/unit_test/provider/user_provider_test.dart
+++ test/unit_test/provider/user_provider_test.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:profile_app/db/user_reopsitory.dart';
+import 'package:profile_app/model/user.dart';
 import 'package:profile_app/values/providers.dart';
 
 import '../../lib/riverpod/future_provider.dart';
@@ -15,15 +16,15 @@ void main() {
   group('userProvider', () {
     late ProviderContainer container;
     late MockListener listener;
-    late MockIUserRepository userRepository;
+    late MockIUserRepository mockUserRepository;
 
     setUp(() async {
       listener = MockListener();
-      userRepository = MockIUserRepository();
+      mockUserRepository = MockIUserRepository();
       container = buildProviderContainerForTest(
         addTearDown: addTearDown,
         overrides: [
-          userRepoProvider.overrideWithValue(userRepository),
+          userRepoProvider.overrideWithValue(mockUserRepository),
         ]
       );
     });
@@ -35,6 +36,30 @@ void main() {
           container.read(authNotifireProvider.notifier).state++;
         }
       );
+    });
+
+    test('return user from repo', () async {
+      when(mockUserRepository.getCurrentUser()).thenAnswer(
+        (_) async => User(name: "John Doe", email: "john@example.com")
+      );
+      container.listen(userProvider, listener, fireImmediately: true);
+
+      await awaitToCompleteProviderFuture(container, userProvider);
+
+      verifyInitialAsyncLoadingCall(listener);
+      verifyAsyncDataCallHavingValue(
+        listener,
+        equals(User(name: 'John Doe', email: "john@example.com"))
+      );
+      verifyNoMoreInteractions(listener);
+    });
+
+    test('called repository getCurrentUser once', () async {
+      container.listen(userProvider, listener, fireImmediately: true);
+      
+      await awaitToCompleteProviderFuture(container, userProvider);
+      
+      verify(mockUserRepository.getCurrentUser()).called(1);
     });
   });
 }
